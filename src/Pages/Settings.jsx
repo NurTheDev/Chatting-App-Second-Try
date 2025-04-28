eca898e2b0a1953d5ee145b93c74817f8ce453d9
NurTheDev <nur756.islam@gmail.com>
2025-04-29 01:20:13 +0600

    added settings pages

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -6,7 +6,7 @@ import {RiMessage3Fill} from "react-icons/ri";
 import {TbPhotoPlus} from "react-icons/tb";
 import {IoMdHelpCircleOutline} from "react-icons/io";
 import {VscColorMode} from "react-icons/vsc";
-import {getAuth, updateProfile} from "firebase/auth";
+import {EmailAuthProvider, getAuth, reauthenticateWithCredential, updatePassword, updateProfile} from "firebase/auth";
 import {toast} from "react-toastify";
 import {getDatabase, ref, update} from "firebase/database";
 import UnderConstruction from "../Common Component/UnderConstruction.jsx";
@@ -34,10 +34,17 @@ function Settings() {
         firstName: "",
         lastName: "",
         bio: "",
+        password: "",
+        confirmPassword: "",
     })
     const auth = getAuth();
     const db = getDatabase();
     const [currentUser, setCurrentUser] = useState([]);
+    const [password, setPassword] = useState({
+        password: "",
+        confirmPassword: "",
+        currentPassword: "",
+    });
     useEffect(() => {
         fetchData((userData) => {
             setCurrentUser(userData);
@@ -103,11 +110,39 @@ function Settings() {
                 }))
             }
         }
+        if (id === "password" || id === "confirmPassword" || id === "currentPassword") {
+            setPassword((prevState) => ({
+                ...prevState,
+                [id]: value,
+            }))
+            if (value === "") {
+                setError((prevState) => ({
+                    ...prevState,
+                    [id]: `${id === "password" ? "Password" : id === "confirmPassword" ? "Confirm Password" : "Current Password"} must be filled`,
+                }))
+            } else if (id === "password" && value.length < 6) {
+                setError((prevState) => ({
+                    ...prevState,
+                    [id]: "Password must be at least 6 characters",
+                }));
+            } else if (id === "confirmPassword" && value !== password.password) {
+                setError((prevState) => ({
+                    ...prevState,
+                    [id]: "Password and Confirm Password must be same",
+                }))
+            } else {
+                setError((prevState) => ({
+                    ...prevState,
+                    [id]: "",
+                }))
+            }
+        }
+
     }
     const handleSubmit = (e) => {
         e.preventDefault();
         const {firstName, lastName} = name;
-        if (error.firstName || error.lastName || error.bio) {
+        if (error.firstName || error.lastName || error.bio || error.password || error.confirmPassword) {
             toast.error("Please fix the errors before submitting", {
                 position: "top-right",
                 autoClose: 5000,
@@ -185,8 +220,73 @@ function Settings() {
                 });
             })
         }
-    }
+        if (password.currentPassword && password.password && password.confirmPassword &&
+            !error.currentPassword && !error.password && !error.confirmPassword) {
+            const credential = EmailAuthProvider.credential(
+                auth.currentUser.email,
+                password.currentPassword
+            )
+            reauthenticateWithCredential(auth.currentUser, credential).then(() => {
+                updatePassword(auth.currentUser, password).then(() => {
+                    setSettings("")
+                    toast.success("Password updated successfully", {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                    });
+                    setPassword({
+                        currentPassword: "",
+                        password: "",
+                        confirmPassword: "",
+                    });
+                }).catch((error) => {
+                    console.log("Error updating password", error);
+                    let errorMessage = "Error updating password";
+                    if (error.code === "auth/weak-password") {
+                        errorMessage = "Password should be at least 6 characters";
+                    } else if (error.code === "auth/invalid-email") {
+                        errorMessage = "Invalid email address";
+                    } else if (error.code === "auth/user-not-found") {
+                        errorMessage = "User not found";
+                    }
+                    toast.error(errorMessage, {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                    });
+                })
+            }).catch((error) => {
+                console.log("Error reauthenticating user", error);
+                let errorMessage = "Error reauthenticating user";
+                if (error.code === "auth/wrong-password") {
+                    errorMessage = "Wrong password";
+                } else if (error.code === "auth/user-not-found") {
+                    errorMessage = "User not found";
+                }
+                toast.error(errorMessage, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+            })
 
+        }
+    }
     return (
         <div>
             <Searchbar/>
@@ -233,7 +333,6 @@ function Settings() {
                                 <h3 className={"text-xl font-poppins"}>{item.title}</h3>
                             </li>
                         ))}
-
                     </ul>) : settings === "editName" ? (<div>
                         <form className="max-w-sm mx-auto mt-10 font-poppins" onSubmit={handleSubmit}>
                             <div className="mb-5">
@@ -272,7 +371,46 @@ function Settings() {
                             <button type="submit"
                                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit
                             </button>
-                        </form>) : settings === "editProfile" ? (<UnderConstruction/>) : ""}
+                        </form>) : settings === "editProfile" ? (
+                        <UnderConstruction/>) : settings === "changePassword" ? (
+                        <form className="max-w-sm mx-auto mt-10 font-poppins" onSubmit={handleSubmit}>
+                            <div className="mb-5">
+                                <label htmlFor="currentPassword"
+                                       className="block mb-2 text-lg font-semibold text-gray-900 ">Current
+                                    Password</label>
+                                <input type="password" id="currentPassword"
+                                       value={password.currentPassword}
+                                       onChange={handleInputChange}
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                       placeholder="Enter your current password" required/>
+                                {error.currentPassword &&
+                                    <p className="text-red-500 text-sm">{error.currentPassword}</p>}
+                            </div>
+                            <div className="mb-5">
+                                <label htmlFor="password"
+                                       className="block mb-2 text-lg font-semibold text-gray-900 ">Password</label>
+                                <input type="password" id="password"
+                                       onChange={handleInputChange}
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                       placeholder="Enter your new password" required/>
+                                {error.password && <p className="text-red-500 text-sm">{error.password}</p>}
+                            </div>
+                            <div className="mb-5">
+                                <label htmlFor="confirmPassword"
+                                       className="block mb-2 text-lg font-semibold text-gray-900">Confirm
+                                    Password</label>
+                                <input type="password" id="confirmPassword"
+                                       onChange={handleInputChange}
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                       placeholder="Confirm your new password" required/>
+                                {error.confirmPassword &&
+                                    <p className="text-red-500 text-sm">{error.confirmPassword}</p>}
+                            </div>
+
+                            <button type="submit"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit
+                            </button>
+                        </form>) : settings === "help" ? (<UnderConstruction/>) : ""}
 
                 </div>
             </div>
